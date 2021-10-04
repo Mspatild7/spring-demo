d9494ccafcd236dc4ef88db9c1cdbb501957cb52
hreshi <hkale746@gmail.com>
2021-10-04 18:56:52 +0530

    first commit

---
+++ src/main/java/com/hreshi/authsystem/Login.java
@@ -0,0 +1,24 @@
+package com.hreshi.authsystem;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class Login {
+	
+	@GetMapping("/login")
+	public String login(@RequestParam(name="name") String name, Model model) {
+		model.addAttribute("username", name);
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login(@RequestParam(name="username")String username, @RequestParam(name="password") String password, Model model) {
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		return "login";
+	}
+}
